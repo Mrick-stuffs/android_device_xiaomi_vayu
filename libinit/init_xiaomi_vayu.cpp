8dcd7f495f27f5619d2327514cb83180e95770ca
aamirr-ali <amirwide@gmail.com>
2024-04-12 02:48:27 +0530

    vayu:  sm8250-common:Add properties for `MATRIXX`
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- libinit/init_xiaomi_vayu.cpp
+++ libinit/init_xiaomi_vayu.cpp
@@ -8,6 +8,25 @@
 #include <libinit_variant.h>
 
 #include "vendor_init.h"
+#include <cstdlib>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
+#include <android-base/properties.h>
+#include "property_service.h"
+
+using android::base::GetProperty;
+using std::string;
+
+void property_override(string prop, string value)
+{
+    auto pi = (prop_info*) __system_property_find(prop.c_str());
+
+    if (pi != nullptr)
+        __system_property_update(pi, value.c_str(), value.size());
+    else
+        __system_property_add(prop.c_str(), prop.size(), value.c_str(), value.size());
+}
 
 #define FINGERPRINT "POCO/vayu_global/vayu:13/RKQ1.200826.002/V14.0.3.0.TJUMIXM:user/release-keys"
 
@@ -62,4 +81,9 @@ static const std::vector<variant_info_t> variants = {
 void vendor_load_properties() {
     set_dalvik_heap();
     search_variant(variants);
+
+    // Misc
+    property_override("ro.matrixx.chipset", "Snapdragon® 860");
+    property_override("ro.matrixx.device", "vayu");
+    property_override("ro.matrixx.maintainer", "Mrick343");
 }
